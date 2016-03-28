861af13f597740dad84c892800f9722701bfaa2f
Karl Mohring <leenix48@gmail.com>
2016-03-28 22:26:17 +1000

    Fixed the 6h timer - Split the timer into minutely intervals to avoid overflows - Added some empty functions for current and voltage measurements

--- src/CatPalaceLighting.ino
+++ src/CatPalaceLighting.ino
@@ -2,6 +2,7 @@
 #include <Logging.h>
 #include "BH1750FVI.h"
 #include "config.h"
+#include <LiquidCrystal.h>
 
 BH1750FVI light_sensor;
 int light_level;
@@ -9,7 +10,14 @@ int light_level;
 SimpleTimer timer;
 int light_timeout = -1;
 int threshold_timeout = -1;
+int remaining_minutes = 0;
 
+LiquidCrystal lcd(LCD_RS_PIN,
+    LCD_ENABLE_PIN,
+    LCD_DATA_0_PIN,
+    LCD_DATA_1_PIN,
+    LCD_DATA_2_PIN,
+    LCD_DATA_3_PIN);
 
 void setup(){
     Log.Init(LOG_LEVEL_VERBOSE, 57600);
@@ -17,6 +25,7 @@ void setup(){
 
     start_lights();
     start_light_sensor();
+    lcd.begin(16, 2);
 }
 
 
@@ -72,14 +81,15 @@ void check_light_sensor(){
         switch_lights(false);
         timer.restartTimer(threshold_timeout);
         timer.disable(light_timeout);
+        remaining_minutes = 0;
     }
     else{
         // Below the threshold - Start threshold countdown/leave it alone
-        if (threshold_timeout < 0){
-            threshold_timeout = timer.setTimeout(THRESHOLD_TIMEOUT, begin_light_time);
+        if (timer.isEnabled(threshold_timeout)){
+            timer.restartTimer(threshold_timeout);
         }
         else{
-            timer.restartTimer(threshold_timeout);
+            threshold_timeout = timer.setTimeout(THRESHOLD_TIMEOUT, begin_light_time);
         }
     }
 }
@@ -94,13 +104,32 @@ void begin_light_time(){
     if (light_timeout > 0){
         timer.restartTimer(light_timeout);
     }else{
-        light_timeout = timer.setTimeout(ON_TIME, end_light_time);
+        light_timeout = timer.setTimeout(ONE_MINUTE, end_light_time);
+    }
+
+    remaining_minutes = LIGHT_ON_TIMER_MINUTES;
+}
+
+
+void check_light_timer(){
+    remaining_minutes--;
+    if (remaining_minutes <= 0){
+        light_timeout = timer.setTimeout(ONE_MINUTE, check_light_timer);
     }
 }
 
 
 void end_light_time(){
     switch_lights(false);
-
     light_timeout = -1;
+}
+
+void check_power(){}
+
+float get_voltage(){
+
+}
+
+float get_current(){
+    
 }
--- src/config.h
+++ src/config.h
@@ -3,10 +3,18 @@
 // Globals
 const long CHECK_SENSOR_INTERVAL = 1000;
 const long THRESHOLD_TIMEOUT = 10*60*1000;
-const long ON_TIME = 6*60*60*1000;
+const int LIGHT_ON_TIMER_MINUTES = 360;
+const int ONE_MINUTE = 60000;
 const int RED_PIN = 5;
 const int GREEN_PIN = 3;
 const int BLUE_PIN = 6;
+
+const int LCD_RS_PIN = 4;
+const int LCD_ENABLE_PIN = 7;
+const int LCD_DATA_0_PIN = 8;
+const int LCD_DATA_1_PIN = 9;
+const int LCD_DATA_2_PIN =10;
+const int LCD_DATA_3_PIN = 11;
 
 const int MIN_LIGHT_THRESHOLD = 20;
 
